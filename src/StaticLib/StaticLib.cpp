a1b74c688f2f2f532159c8244ca8ac9173d527dc
satomoti <g2227039@st.t-kougei.ac.jp>
2022-12-11 23:34:42 +0900

    テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -69,7 +69,29 @@ bool add(tree* t, int key, const char* value)
 	}
 
 	// Todo: t->rootの下にkeyの値の大小でleftかrightを切り替えながらpを追加する処理を実装する
-
+	node* current = t->root;
+	while(true)
+	{
+		if (key > current->key) {
+			if (current->right == NULL) {
+				current->right = p;
+				break;
+			}
+			current = current->right;
+		}
+		else if (key < current->key) {
+			if (current->left == NULL) {
+				current->left = p;
+				break;
+			}
+			current = current->left;
+		}
+		else {
+			memcpy_s(current->value, sizeof(current->value), value, sizeof(current->value));
+			free(p);
+			break;
+		}
+	}
 	return true;
 }
 
@@ -77,11 +99,41 @@ bool add(tree* t, int key, const char* value)
 const char* find(const tree* t, int key)
 {
 	// ToDo: 実装する
+	if (t == NULL || t->root == NULL)return NULL;
+	node* current = t->root;
+	while(current != NULL){
+		if (key > current->key) {
+			current = current->right;
+		}
+		else if (key < current->key) {
+			current = current->left;
+		}
+		else {
+			return current->value;
+		}
+	}
 	return NULL;
+}
+
+void callbackcome(const node* data, void (*func)(const node* p))
+{
+	if (data->left == NULL && data->right == NULL) {
+		func(data); return;
+	}
+	if (data->left != NULL) {
+		callbackcome(data->left, func);
+	}
+	func(data);
+	if (data->right != NULL) {
+		callbackcome(data->right, func);
+	}
 }
 
 // keyの小さな順にコールバック関数funcを呼び出す
 void search(const tree* t, void (*func)(const node* p))
 {
 	// ToDo: 実装する
+	if (t == NULL || t->root == NULL)return;
+	callbackcome(t->root, func);
 }
+
