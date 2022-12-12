27b3098ec3238b6f51b5ee1e561dc993a6f7a61f
zakitowa1210 <g2227077@st.t-kougei.ac.jp>
2022-12-12 11:47:06 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -8,14 +8,14 @@
 // 2分木の初期化
 void initialize(tree* t)
 {
-	if (t == NULL) return;
+	if (!t) return;
 
 	t->root = NULL;
 }
 
 static void release_recursive(node* n)
 {
-	if (n == NULL) return;
+	if (!n) return;
 
 	if (n->right) {
 		release_recursive(n->right);
@@ -33,7 +33,7 @@ static void release_recursive(node* n)
 // 使用メモリの全解放
 void finalize(tree* t)
 {
-	if (t == NULL) return;
+	if (!t) return;
 
 	release_recursive(t->root);
 	t->root = NULL;
@@ -44,7 +44,7 @@ static node* generate(int key, const char* value)
 {
 	node* p = (node*)malloc(sizeof(node));
 
-	if (p == NULL) return NULL;
+	if (!p) return NULL;
 
 	p->key = key;
 	int n = (int)strlen(value);
@@ -58,18 +58,41 @@ static node* generate(int key, const char* value)
 // keyの値を見てノードを追加する
 bool add(tree* t, int key, const char* value)
 {
-	if (t == NULL) return false;
+	if (!t) return false;
 
 	node* p = generate(key, value);
-	if (p == NULL) return false;// メモリ確保できなかった。
+	if (!p) return false;// メモリ確保できなかった。
 
-	if (t->root == NULL) {
+	if (!t->root ) {
 		t->root = p;
 		return true;
 	}
 
 	// Todo: t->rootの下にkeyの値の大小でleftかrightを切り替えながらpを追加する処理を実装する
+	node* data;
+	data = t->root;
+	while (true) {
+		if (data->key > key) {
+			if (!data->left) {
+				data->left = p;
+				break;
+			}
+			data = data->left;
+		}
+		else if (data->key < key) {
+			if (!data->right) {
+				data->right = p;
+				break;
+			}
+			data = data->right;
+		}
+		else {
+			memcpy(data, p, sizeof(node));
+			free(p);
+			break;
+		}
 
+	}
 	return true;
 }
 
@@ -77,6 +100,27 @@ bool add(tree* t, int key, const char* value)
 const char* find(const tree* t, int key)
 {
 	// ToDo: 実装する
+	if (!t || !t->root)return NULL;
+	node* data;
+	data = t->root;
+	if (data->key == key) {
+		return data->value;
+	}
+	while (data) {
+
+		if (data->key > key) {
+			if (!data->left)return NULL;
+			data = data->left;
+		}
+		else {
+			if (!data->right)return NULL;
+			data = data->right;
+		}
+		if (data->key == key) {
+			return data->value;
+		}
+
+	}
 	return NULL;
 }
 
@@ -84,4 +128,29 @@ const char* find(const tree* t, int key)
 void search(const tree* t, void (*func)(const node* p))
 {
 	// ToDo: 実装する
+	if (!t || !t->root)return;
+
+	if (t->root->left) {
+		small_order(t->root->left, func);
+	}
+	func(t->root);
+
+	if (t->root->right) {
+		small_order(t->root->right, func);
+	}
+}
+
+void small_order(const node* data, void (*func)(const node* p)) {
+	if (!data->left && !data->right) {
+		func(data);
+		return;
+	}
+	if (data->left) {
+		small_order(data->left, func);
+	}
+	func(data);
+
+	if (data->right) {
+		small_order(data->right, func);
+	}
 }
